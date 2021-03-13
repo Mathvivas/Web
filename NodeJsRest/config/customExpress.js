090e287f201cadadbd3030746f34f7aaf9744640
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-03-13 17:42:20 -0300

    Utilização do bodyParser

--- NodeJsRest/config/customExpress.js
+++ NodeJsRest/config/customExpress.js
@@ -1,9 +1,12 @@
-const express = require('express')  // npm install express
-const consign = require('consign')
+const express = require('express')          // npm install express
+const consign = require('consign')          // npm install consign
+const bodyParser = require('body-parser')   // npm install body-parser
 
 module.exports = () => {
     const app = express()   // Executar um servidor
 
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
     consign().include('controllers').into(app)  // Incluindo os controllers dentro deste app
 
     return app
