d53f32f04d309691a52f4f534c9e04d1d793a743
Matheus Lopes Vivas <mathvivas@hotmail.com>
2021-03-14 17:48:58 -0300

    Implementaçao de Data

--- NodeJsRest/infraestrutura/tabelas.js
+++ NodeJsRest/infraestrutura/tabelas.js
@@ -8,7 +8,8 @@ class Tabelas {
     criarAtendimentos() {
         const sql = 
         'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, ' +  
-        'pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes TEXT, ' +
+        'pet varchar(20), servico varchar(20) NOT NULL, data DATETIME NOT NULL, dataCriacao DATETIME NOT NULL' + 
+        'status varchar(20) NOT NULL, observacoes TEXT, ' +
         'PRIMARY KEY(id))'
 
         this.conexao.query(sql, (erro) => {
--- NodeJsRest/models/atendimentos.js
+++ NodeJsRest/models/atendimentos.js
@@ -1,7 +1,11 @@
 const conexao = require('../infraestrutura/conexao')
+const moment = require('moment')
 
 class Atendimento {
     adicionar(atendimento) {
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const atendimentoDatado = { ...atendimento, dataCriacao, data }   // array -> tudo que estiver dentro de atendimento e a data
         const sql = 'INSERT INTO Atendimentos SET ?'
         
         conexao.query(sql, atendimento, (erro, resultados) => {
