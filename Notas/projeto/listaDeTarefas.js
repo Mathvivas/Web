25d220d3058941bc0563aa8b6431a423affc2560
Mathvivas <mathvivas@hotmail.com>
2021-03-31 16:28:56 -0300

    Manipulando o DOM

--- Notas/projeto/listaDeTarefas.js
+++ Notas/projeto/listaDeTarefas.js
@@ -1,11 +1,22 @@
 const criarTarefa = (evento) => {
     evento.preventDefault()     // Previne o form de enviar o dado (recarregar a página)
+    
+    const lista = document.querySelector('[data-list]')
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
-    input.valor = " "
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')        // Adiciona uma classe ao elemento
+
+    const conteudo = `<p class="content">${valor}</p>`
+
+    tarefa.innerHTML = conteudo
+    lista.appendChild(tarefa)
+
+    input.value = " "
 }
 
 // seleciona a tag que possui esse data-attribute (data-form-button)
 const novaTarefa = document.querySelector('[data-form-button]')
 
-novaTarefa.addEventListener('click', (evento) => criarTarefa)
+novaTarefa.addEventListener('click', criarTarefa)
