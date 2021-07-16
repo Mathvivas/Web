c79e210bca6dd8d03c75840aa5524fd41ac38e48
Mathvivas <mathvivas@hotmail.com>
2021-07-16 14:44:49 -0300

    Implementaçao do Login

--- imersao-react/alurakut/pages/index.js
+++ imersao-react/alurakut/pages/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import nookies from 'nookies'
+import jwt from 'jsonwebtoken'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
 import { AlurakutMenu, OrkutNostalgicIconSet, AlurakutProfileSidebarMenuDefault } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
+
+
+/*
+    const githubUser = jwt.decode(token).githubUser
+                      =
+    const { githubUser } = jwt.decode(token)
+*/
+
 
 function ProfileSidebar(propriedades) {
   console.log(propriedades);
@@ -45,8 +55,8 @@ function ProfileRelationsBox(propriedades) {
   )
 }
 
-export default function Home() {
-  const usuarioAleatorio = 'Mathvivas';
+export default function Home(props) {
+  const usuarioAleatorio = props.githubUser;
   const [comunidades, setComunidades] = React.useState([])
 
   const pessoasFavoritas = [
@@ -208,4 +218,31 @@ export default function Home() {
       </MainGrid>
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+  const cookies = nookies.get(context)
+  const token = cookies.USER_TOKEN
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
+    headers: {
+      Authorization: token
+    }
+  })
+  .then((resposta) => resposta.json())
+
+  if ( !isAuthenticated ) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  }
+
+  const { githubUser } = jwt.decode(token)
+  return {
+    props: {
+      githubUser
+    },
+  }
 }
