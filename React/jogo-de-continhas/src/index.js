ac32437cfaba50049e2f9f8da99dc6557e22e6a9
Mathvivas <mathvivas@hotmail.com>
2021-09-04 13:37:11 -0300

    Inicio do Jogo

---
+++ React/jogo-de-continhas/src/index.js
@@ -0,0 +1,21 @@
+import 'primereact/resources/themes/saga-blue/theme.css'
+import 'primereact/resources/primereact.min.css'
+import 'primereact/primeicons.css'
+import 'primeflex/primeflex.css'
+
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import Jogo from './Jogo'
+
+export default class App extends Component {
+    render() {
+        return (
+            <Jogo />
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
