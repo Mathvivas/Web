61ff99148016ef664cc8247dfde55c1268ca4abc
Mathvivas <mathvivas@hotmail.com>
2021-09-04 11:27:30 -0300

    Utilizaçao de Loading, components e refatoraçao

--- React/estacoes-climaticas/src/index.js
+++ React/estacoes-climaticas/src/index.js
@@ -1,20 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import EstacaoClimatica from './EstacaoClimatica'
+import Loading from './Loading'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 class App extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = {
-            lat: null,
-            lng: null,
-            estacao: null,
-            data: null,
-            icone: null,
-            mensagemErro: null
-        }
+        console.log('Construtor entrou em execução')
+    }
+
+    state = {
+        lat: null,
+        lng: null,
+        estacao: null,
+        data: null,
+        icone: null,
+        mensagemErro: null
+    }
+
+    componentDidMount() {
+        console.log("ComponentDidMount")
+        this.obterLocalizacao()
+    }
+
+    componentDidUpdate() {
+        console.log("ComponentDidUpdate")
+    }
+
+    componentWillUnmount() {
+        console.log("ComponentWillUnmount")
     }
 
     obterEstacao = (data, latitude) => {
@@ -68,6 +85,7 @@ class App extends React.Component {
     }
 
     render() {
+        console.log('Render')
         //this.obterLocalizacao()
         // let texto = 'Clique no botão para saber sua estação climática'
         // if ( this.state.lat ) {
@@ -77,30 +95,24 @@ class App extends React.Component {
             <div className="container mt-2">
                 <div className="row justify-content-center">
                     <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-body">
-                                <div className="d-flex align-items-center border mb-2">
-                                    <i className={`fas fa-5x ${this.state.icone}`}></i>
-                                    <p className="w-75 ms-3 text-center fs-1">{this.state.estacao}</p>
-                                </div>
-                                <div>
-                                    <p className="text-center">
-                                        {
-                                            this.state.lat ?
-                                            `Coordenadas: ${this.state.lat}, ${this.state.lng}. Data: ${this.state.data}`
-                                            :
-                                            this.state.mensagemErro ?
-                                            `${this.state.mensagemErro}`
-                                            :
-                                            `Permita a localização para saber a sua estação climática`
-                                        }
-                                    </p>
-                                </div>
-                                <button onClick={this.obterLocalizacao} className="btn btn-outline-primary w-100 mt-2">
-                                    Qual a minha estação?
-                                </button>
-                            </div>
-                        </div>
+                        {
+                            (!this.state.lat && !this.mensagemErro) ?
+                            <Loading mensagem="Por favor, responda à solicitação de localização"/>
+                            :
+                            this.state.mensagemErro ?
+                            <p className="border rounded p-2 fs-1 text-center">
+                                É preciso dar permissão de acesso à localização. Atualize a página e tente de novo.
+                            </p>
+                            :
+                            <EstacaoClimatica 
+                                icone={this.state.icone}
+                                estacao={this.state.estacao}
+                                lat={this.state.lat}
+                                lng={this.state.lng}
+                                mensagemErro={this.state.mensagemErro}
+                                obterLocalizacao={this.obterLocalizacao}
+                            />
+                        }
                     </div>
                 </div>
             </div>
