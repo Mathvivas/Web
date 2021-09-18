45d994c57a6809ef86e2a745365ec16d4724d06c
Mathvivas <mathvivas@hotmail.com>
2021-09-18 12:54:23 -0300

    Utilizaçao da API do Pexels e do .env

--- React/busca-figuras/src/components/App.js
+++ React/busca-figuras/src/components/App.js
@@ -1,10 +1,23 @@
+// npm i primereact primeicons react-transition-group primeflex
+
 import React, { Component } from 'react'
+import Busca from './Busca'
+import env from "react-dotenv"
 
 export default class App extends Component {
+    onBuscaRealizada = (termo) => {
+        console.log(termo)
+    }
+
     render() {
         return (
-            <div>
-                Modelo PrimeReact e PrimeFlex
+            <div className="grid justify-content-center m-auto w-9 border-round border-1 border-400">
+                <div className="col-12">
+                    <h1>Exibir uma Lista de ...</h1>
+                </div>
+                <div className="col-8">
+                    <Busca onBuscaRealizada={this.onBuscaRealizada}/>
+                </div>
             </div>
         )
     }
