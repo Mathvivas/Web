ddd6072ab27f0684ab85d1de62f43139e5e5f54e
Mathvivas <mathvivas@hotmail.com>
2021-09-21 13:20:42 -0300

    Testando Componentes e Funçoes

--- React/react-testes/src/App.test.js
+++ React/react-testes/src/App.test.js
@@ -2,7 +2,7 @@
 // 2) npm test --> para rodar os testes
 
 import React from 'react';
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 
 import App, { calcularNovoSaldo } from './App'
 
@@ -35,6 +35,24 @@ describe('Componente principal', () => {
             }
             const novoSaldo = calcularNovoSaldo(valores, 150)
             expect(novoSaldo).toBe(100)
+        })
+
+        it ('que é um saque, a transação deve ser realizada', () => {
+            //const { getByText, getByTestId, getByLabelText } = render(<App />)
+            render(<App />)
+
+            const saldo = screen.getByText('R$ 1000')
+            const transacao = screen.getByLabelText('Saque')
+            const valor = screen.getByTestId('valor')
+            const botaoTransacao = screen.getByText('Realizar operação')
+
+            expect(saldo.textContent).toBe('R$ 1000')
+
+            fireEvent.click(transacao, { target: { value: 'saque' } })
+            fireEvent.change(valor, { target: { value: 10 }})
+            fireEvent.click(botaoTransacao)
+
+            expect(saldo.textContent).toBe('R$ 990')
         })
     })
 })
